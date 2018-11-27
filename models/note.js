56f29ed60b0da3d7a63f81df298b8669abb5f42d
Michael McKay <mamckay89@gmail.com>
2018-11-27 13:56:52 -0700

    More updates on the scratch file queries

--- models/note.js
+++ models/note.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: String,
-  createdAt: Date.now(),
+  createdAt: Date,
   updatedAt: Date,
 });
 
